421133463e4e03f5d6a1fcd80774e17f3aa94ab8
wangyuanti <wangyuanti@163.com>
2019-07-29 10:50:39 +0800

    整合

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,9 @@
 export default {
     domain:'http://192.168.20.96:8080',
-
+    //本地
+    // domain:'http://tp.welltrend.com',
 
     img: 'http://192.168.20.96:8080/uploads/',
+    //本地
+    // img: 'http://tp.welltrend.com/uploads/',
 }
